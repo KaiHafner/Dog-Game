5a76d4ae9f494a79308a6bf1d4fdc10b3ff9597b
Kai <kai.hafner@outlook.com>
2024-11-28 12:55:37 +0000

    removed broken quest hopefully

--- Source/DogGame/QuestTrackerWidget.cpp
+++ Source/DogGame/QuestTrackerWidget.cpp
@@ -1,18 +1,18 @@
-#include "QuestTrackerWidget.h"
-#include "Components/TextBlock.h"
-#include "Quest.h"
-
-void UQuestTrackerWidget::UpdateUI(UQuest* ActiveQuest)
-{
-    if (ActiveQuest)
-    {
-        QuestNameText->SetText(FText::FromString(ActiveQuest->QuestName));
-        QuestDescriptionText->SetText(FText::FromString(ActiveQuest->QuestDescription));
-        GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("ACTIVE"));
-    }
-    else
-    {
-        QuestNameText->SetText(FText::FromString("No Active Quest"));
-        QuestDescriptionText->SetText(FText::FromString(""));
-    }
-}
+//#include "QuestTrackerWidget.h"
+//#include "Components/TextBlock.h"
+//#include "Quest.h"
+//
+//void UQuestTrackerWidget::UpdateUI(UQuest* ActiveQuest)
+//{
+//    if (ActiveQuest)
+//    {
+//        QuestNameText->SetText(FText::FromString(ActiveQuest->QuestName));
+//        QuestDescriptionText->SetText(FText::FromString(ActiveQuest->QuestDescription));
+//        GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("ACTIVE"));
+//    }
+//    else
+//    {
+//        QuestNameText->SetText(FText::FromString("No Active Quest"));
+//        QuestDescriptionText->SetText(FText::FromString(""));
+//    }
+//}
--- Source/DogGame/QuestTrackerWidget.h
+++ Source/DogGame/QuestTrackerWidget.h
@@ -1,24 +1,24 @@
-#pragma once
-
-#include "CoreMinimal.h"
-#include "Blueprint/UserWidget.h"
-#include "QuestTrackerWidget.generated.h"
-
-class UQuest;
-
-UCLASS()
-class DOGGAME_API UQuestTrackerWidget : public UUserWidget
-{
-    GENERATED_BODY()
-
-public:
-    UFUNCTION(BlueprintCallable, Category = "Quest Tracker")
-    void UpdateUI(UQuest* ActiveQuest);
-
-protected:
-    UPROPERTY(meta = (BindWidget))
-    class UTextBlock* QuestNameText;
-
-    UPROPERTY(meta = (BindWidget))
-    class UTextBlock* QuestDescriptionText;
-};
+//#pragma once
+//
+//#include "CoreMinimal.h"
+//#include "Blueprint/UserWidget.h"
+//#include "QuestTrackerWidget.generated.h"
+//
+//class UQuest;
+//
+//UCLASS()
+//class DOGGAME_API UQuestTrackerWidget : public UUserWidget
+//{
+//    GENERATED_BODY()
+//
+//public:
+//    UFUNCTION(BlueprintCallable, Category = "Quest Tracker")
+//    void UpdateUI(UQuest* ActiveQuest);
+//
+//protected:
+//    UPROPERTY(meta = (BindWidget))
+//    class UTextBlock* QuestNameText;
+//
+//    UPROPERTY(meta = (BindWidget))
+//    class UTextBlock* QuestDescriptionText;
+//};
